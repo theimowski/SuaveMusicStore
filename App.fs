54bb0f11877fcdd4295eb719ad4558e8ad984684
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-01 21:16:02 +0200

    details route parametrized with id; browse route requires genre in query

--- App.fs
+++ App.fs
@@ -2,14 +2,22 @@
 open Suave.Http
 open Suave.Http.Successful
 open Suave.Http.Applicatives
+open Suave.Http.RequestErrors
+open Suave.Types
 open Suave.Web
+
+let browse =
+    request (fun r ->
+        match r.queryParam "genre" with
+        | Choice1Of2 genre -> OK (sprintf "Genre: %s" genre)
+        | Choice2Of2 msg -> BAD_REQUEST msg)
 
 let webPart = 
     choose [
         path "/" >>= (OK "Home")
         path "/store" >>= (OK "Store")
-        path "/store/browse" >>= (OK "Genre")
-        path "/store/details" >>= (OK "Details")
+        path "/store/browse" >>= browse
+        pathScan "/store/details/%d" (fun id -> OK (sprintf "Details %d" id))
     ]
 
 startWebServer defaultConfig webPart
