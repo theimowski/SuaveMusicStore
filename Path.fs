f1aa25b21fb24315410bb1e119ebca6492277ab3
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-06 14:06:23 +0200

    add Path.fs with paths to home and Store module

---
+++ Path.fs
@@ -0,0 +1,10 @@
+﻿module SuaveMusicStore.Path
+
+type IntPath = PrintfFormat<(int -> string),unit,string,string,int>
+
+let home = "/"
+
+module Store =
+    let overview = "/store"
+    let browse = "/store/browse"
+    let details : IntPath = "/store/details/%d"
