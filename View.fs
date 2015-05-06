5a458cc12d03a3381887211126bc8d8fd3c0832d
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-06 14:01:49 +0200

    simple index view

--- View.fs
+++ View.fs
@@ -1,2 +1,28 @@
 ﻿module SuaveMusicStore.View
 
+open Suave.Html
+
+let divId id = divAttr ["id", id]
+let h1 xml = tag "h1" [] xml
+let aHref href = tag "a" ["href", href]
+
+let index = 
+    html [
+        head [
+            title "Suave Music Store"
+        ]
+
+        body [
+            divId "header" [
+                h1 (aHref "/" (text "F# Suave Music Store"))
+            ]
+
+            divId "footer" [
+                text "built with "
+                aHref "http://fsharp.org" (text "F#")
+                text " and "
+                aHref "http://suave.io" (text "Suave.IO")
+            ]
+        ]
+    ]
+    |> xmlToString
