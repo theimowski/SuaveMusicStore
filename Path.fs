f79d5784a2dfa353082b7a5a9503f80d1f4ea2c1
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-07 22:49:25 +0200

    links to genres for /store

--- Path.fs
+++ Path.fs
@@ -2,9 +2,13 @@
 
 type IntPath = PrintfFormat<(int -> string),unit,string,string,int>
 
+let withParam (key,value) path = sprintf "%s?%s=%s" path key value
+
 let home = "/"
 
 module Store =
     let overview = "/store"
     let browse = "/store/browse"
     let details : IntPath = "/store/details/%d"
+
+    let browseKey = "genre"
