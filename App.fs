1ca74eee6e4224d1e04a9aeb9990b4b20436a823
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-07 22:49:25 +0200

    links to genres for /store

--- App.fs
+++ App.fs
@@ -10,15 +10,15 @@ let html container =
     OK (View.index container)
 
 let browse =
-    request (fun r ->
-        match r.queryParam "genre" with
+    request (fun r -> 
+        match r.queryParam Path.Store.browseKey with
         | Choice1Of2 genre -> html (View.browse genre)
         | Choice2Of2 msg -> BAD_REQUEST msg)
 
 let webPart = 
     choose [
         path Path.home >=> html View.home
-        path Path.Store.overview >=> html View.store
+        path Path.Store.overview >=> html (View.store ["Rock"; "Disco"; "Pop"])
         path Path.Store.browse >=> browse
         pathScan Path.Store.details (fun id -> html (View.details id))
 
--- View.fs
+++ View.fs
@@ -7,13 +7,22 @@ let h1 xml = tag "h1" [] xml
 let h2 s = tag "h2" [] (text s)
 let aHref href = tag "a" ["href", href]
 let cssLink href = linkAttr [ "href", href; " rel", "stylesheet"; " type", "text/css" ]
+let ul xml = tag "ul" [] (flatten xml)
+let li = tag "li" []
 
 let home = [
     h2 "Home"
 ]
 
-let store = [
-    h2 "Store"
+let store genres = [
+    h2 "Browse Genres"
+    p [
+        text (sprintf "Select from %d genres:" (List.length genres))
+    ]
+    ul [
+        for g in genres -> 
+            li (aHref (Path.Store.browse |> Path.withParam (Path.Store.browseKey, g)) (text g))
+    ]
 ]
 
 let browse genre = [
