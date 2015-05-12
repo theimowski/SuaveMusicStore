ac1d9704f4a97d918f39dcec2e731413ee1d57e8
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-12 21:46:52 +0200

    add sql type

--- Db.fs
+++ Db.fs
@@ -1,2 +1,8 @@
 ﻿module SuaveMusicStore.Db
 
+open FSharp.Data.Sql
+
+type Sql = 
+    SqlDataProvider< 
+        "Server=(LocalDb)\\v11.0;Database=SuaveMusicStore;Trusted_Connection=True;MultipleActiveResultSets=true", 
+        DatabaseVendor=Common.DatabaseProviderTypes.MSSQLSERVER >
