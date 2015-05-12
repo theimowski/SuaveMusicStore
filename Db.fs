ccc230d1b8421878032bc0c853526ce853ffaff2
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-12 21:49:28 +0200

    type annotations

--- Db.fs
+++ Db.fs
@@ -6,3 +6,8 @@ type Sql =
     SqlDataProvider< 
         "Server=(LocalDb)\\v11.0;Database=SuaveMusicStore;Trusted_Connection=True;MultipleActiveResultSets=true", 
         DatabaseVendor=Common.DatabaseProviderTypes.MSSQLSERVER >
+
+type DbContext = Sql.dataContext
+type Album = DbContext.``[dbo].[Albums]Entity``
+type Genre = DbContext.``[dbo].[Genres]Entity``
+type AlbumDetails = DbContext.``[dbo].[AlbumDetails]Entity``
