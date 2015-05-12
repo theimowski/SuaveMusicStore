0828640686ca2e1c784618d861ade79f15fc3344
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-12 22:18:54 +0200

    album details from db + view

--- App.fs
+++ App.fs
@@ -29,12 +29,19 @@ let overview = warbler (fun _ ->
     |> View.store 
     |> html)
 
+let details id =
+    match Db.getAlbumDetails id (Db.getContext()) with
+    | Some album ->
+        html (View.details album)
+    | None ->
+        never
+
 let webPart = 
     choose [
         path Path.home >>= html View.home
         path Path.Store.overview >>= overview
         path Path.Store.browse >>= browse
-        pathScan Path.Store.details (fun id -> html (View.details id))
+        pathScan Path.Store.details details
 
         pathRegex "(.*)\.(css|png)" >>= Files.browseHome
     ]
