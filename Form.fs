fc3d0500a2d4447a5174f3db0748cb4e21699c0d
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-23 19:00:57 +0200

    add Form.Logon

--- Form.fs
+++ Form.fs
@@ -16,3 +16,10 @@ let album : Form<Album> =
             DecimalProp ((fun f -> <@ f.Price @>), [ min 0.01M; max 100.0M; step 0.01M ])
             ],
           [])
+
+type Logon = {
+    Username : string
+    Password : Password
+}
+
+let logon : Form<Logon> = Form ([],[])
