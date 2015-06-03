ba7f6dce9c240835ef6434c9b7bad965e691750e
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-06-03 22:16:30 +0200

    link to register on View.logon

--- View.fs
+++ View.fs
@@ -207,6 +207,8 @@ let logon msg = [
     h2 "Log On"
     p [
         text "Please enter your user name and password."
+        aHref Path.Account.register (text " Register")
+        text " if you don't have an account yet."
     ]
 
     divId "logon-message" [
