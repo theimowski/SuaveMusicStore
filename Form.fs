0a4caf18ee0610056f82a55bd2f46ef04856d952
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-06-03 22:43:34 +0200

    Form.checkout

--- Form.fs
+++ Form.fs
@@ -43,3 +43,12 @@ let register : Form<Register> =
             PasswordProp ((fun f -> <@ f.Password @>), [ passwordRegex pattern ] )
             PasswordProp ((fun f -> <@ f.ConfirmPassword @>), [ passwordRegex pattern ] )
             ],[ passwordsMatch ])
+
+type Checkout = {
+    FirstName : string
+    LastName : string
+    Address : string
+    PromoCode : string option
+}
+
+let checkout : Form<Checkout> = Form ([], [])
