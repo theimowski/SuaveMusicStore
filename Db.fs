16d8fec5f3daa46733501090282f95c5894336fd
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-03-20 10:43:02 +0100

    apply changes for Db module

--- Db.fs
+++ Db.fs
@@ -5,33 +5,34 @@ open FSharp.Data.Sql
 
 type Sql = 
     SqlDataProvider< 
-        "Server=(LocalDb)\\v11.0;Database=SuaveMusicStore;Trusted_Connection=True;MultipleActiveResultSets=true", 
-        DatabaseVendor=Common.DatabaseProviderTypes.MSSQLSERVER >
+        ConnectionString      = "Server=127.0.0.1;Database=suavemusicstore;User Id=suave;Password=1234;",
+        DatabaseVendor        = Common.DatabaseProviderTypes.POSTGRESQL,
+        CaseSensitivityChange = Common.CaseSensitivityChange.ORIGINAL >
 
 type DbContext = Sql.dataContext
-type Album = DbContext.``[dbo].[Albums]Entity``
-type Artist = DbContext.``[dbo].[Artists]Entity``
-type Genre = DbContext.``[dbo].[Genres]Entity``
-type AlbumDetails = DbContext.``[dbo].[AlbumDetails]Entity``
-type User = DbContext.``[dbo].[Users]Entity``
-type Cart = DbContext.``[dbo].[Carts]Entity``
-type CartDetails = DbContext.``[dbo].[CartDetails]Entity``
-type BestSeller = DbContext.``[dbo].[BestSellers]Entity``
+type Album = DbContext.``public.albumsEntity``
+type Artist = DbContext.``public.artistsEntity``
+type Genre = DbContext.``public.genresEntity``
+type AlbumDetails = DbContext.``public.albumdetailsEntity``
+type User = DbContext.``public.usersEntity``
+type Cart = DbContext.``public.cartsEntity``
+type CartDetails = DbContext.``public.cartdetailsEntity``
+type BestSeller = DbContext.``public.bestsellersEntity``
 
 let getContext() = Sql.GetDataContext()
 
 let firstOrNone s = s |> Seq.tryFind (fun _ -> true)
 
 let getGenres (ctx : DbContext) : Genre list = 
-    ctx.``[dbo].[Genres]`` |> Seq.toList
+    ctx.Public.Genres |> Seq.toList
 
 let getArtists (ctx : DbContext) : Artist list = 
-    ctx.``[dbo].[Artists]`` |> Seq.toList
+    ctx.Public.Artists |> Seq.toList
 
 let getAlbumsForGenre genreName (ctx : DbContext) : Album list = 
     query { 
-        for album in ctx.``[dbo].[Albums]`` do
-            join genre in ctx.``[dbo].[Genres]`` on (album.GenreId = genre.GenreId)
+        for album in ctx.Public.Albums do
+            join genre in ctx.Public.Genres on (album.Genreid = genre.Genreid)
             where (genre.Name = genreName)
             select album
     }
@@ -39,66 +40,73 @@ let getAlbumsForGenre genreName (ctx : DbContext) : Album list =
 
 let getAlbumDetails id (ctx : DbContext) : AlbumDetails option = 
     query { 
-        for album in ctx.``[dbo].[AlbumDetails]`` do
-            where (album.AlbumId = id)
+        for album in ctx.Public.Albumdetails do
+            where (album.Albumid = id)
             select album
     } |> firstOrNone
 
 let getAlbumsDetails (ctx : DbContext) : AlbumDetails list = 
-    ctx.``[dbo].[AlbumDetails]`` |> Seq.toList
+    ctx.Public.Albumdetails |> Seq.toList
 
 let getBestSellers (ctx : DbContext) : BestSeller list  =
-    ctx.``[dbo].[BestSellers]`` |> Seq.toList
+    query { 
+        for bs in ctx.Public.Bestsellers do 
+            select bs 
+    } |> Seq.toList   
 
 let getAlbum id (ctx : DbContext) : Album option = 
     query { 
-        for album in ctx.``[dbo].[Albums]`` do
-            where (album.AlbumId = id)
+        for album in ctx.Public.Albums do
+            where (album.Albumid = id)
             select album
     } |> firstOrNone
 
 let validateUser (username, password) (ctx : DbContext) : User option =
     query {
-        for user in ctx.``[dbo].[Users]`` do
-            where (user.UserName = username && user.Password = password)
+        for user in ctx.Public.Users do
+            where (user.Username= username && user.Password = password)
             select user
     } |> firstOrNone
 
 let getUser username (ctx : DbContext) : User option = 
     query {
-        for user in ctx.``[dbo].[Users]`` do
-        where (user.UserName = username)
+        for user in ctx.Public.Users do
+        where (user.Username = username)
         select user
     } |> firstOrNone
 
 let getCart cartId albumId (ctx : DbContext) : Cart option =
     query {
-        for cart in ctx.``[dbo].[Carts]`` do
-            where (cart.CartId = cartId && cart.AlbumId = albumId)
+        for cart in ctx.Public.Carts do
+            where (cart.Cartid = cartId && cart.Albumid = albumId)
             select cart
     } |> firstOrNone
 
 let getCarts cartId (ctx : DbContext) : Cart list =
     query {
-        for cart in ctx.``[dbo].[Carts]`` do
-            where (cart.CartId = cartId)
+        for cart in ctx.Public.Carts do
+            where (cart.Cartid = cartId)
             select cart
     } |> Seq.toList
 
 let getCartsDetails cartId (ctx : DbContext) : CartDetails list =
     query {
-        for cart in ctx.``[dbo].[CartDetails]`` do
-            where (cart.CartId = cartId)
+        for cart in ctx.Public.Cartdetails do
+            where (cart.Cartid = cartId)
             select cart
     } |> Seq.toList
 
 let createAlbum (artistId, genreId, price, title) (ctx : DbContext) =
-    ctx.``[dbo].[Albums]``.Create(artistId, genreId, price, title) |> ignore
+    let a = ctx.Public.Albums.Create()
+    a.Artistid <- artistId
+    a.Genreid <- genreId
+    a.Price <- price
+    a.Title <- title
     ctx.SubmitUpdates()
 
 let updateAlbum (album : Album) (artistId, genreId, price, title) (ctx : DbContext) =
-    album.ArtistId <- artistId
-    album.GenreId <- genreId
+    album.Artistid <- artistId
+    album.Genreid <- genreId
     album.Price <- price
     album.Title <- title
     ctx.SubmitUpdates()
@@ -112,7 +120,7 @@ let addToCart cartId albumId (ctx : DbContext)  =
     | Some cart ->
         cart.Count <- cart.Count + 1
     | None ->
-        ctx.``[dbo].[Carts]``.Create(albumId, cartId, 1, DateTime.UtcNow) |> ignore
+        ctx.Public.Carts.Create(albumId, cartId, 1, DateTime.UtcNow) |> ignore
     ctx.SubmitUpdates()
 
 let removeFromCart (cart : Cart) albumId (ctx : DbContext) = 
@@ -122,27 +130,27 @@ let removeFromCart (cart : Cart) albumId (ctx : DbContext) =
 
 let upgradeCarts (cartId : string, username :string) (ctx : DbContext) =
     for cart in getCarts cartId ctx do
-        match getCart username cart.AlbumId ctx with
+        match getCart username cart.Albumid ctx with
         | Some existing ->
             existing.Count <- existing.Count +  cart.Count
             cart.Delete()
         | None ->
-            cart.CartId <- username
+            cart.Cartid <- username
     ctx.SubmitUpdates()
 
 let newUser (username, password, email) (ctx : DbContext) =
-    let user = ctx.``[dbo].[Users]``.Create(email, password, "user", username)
+    let user = ctx.Public.Users.Create(email, password, "user", username)
     ctx.SubmitUpdates()
     user
 
 let placeOrder (username : string) (ctx : DbContext) =
     let carts = getCartsDetails username ctx
     let total = carts |> List.sumBy (fun c -> (decimal) c.Count * c.Price)
-    let order = ctx.``[dbo].[Orders]``.Create(DateTime.UtcNow, total)
+    let order = ctx.Public.Orders.Create(DateTime.UtcNow, total)
     order.Username <- username
     ctx.SubmitUpdates()
     for cart in carts do
-        let orderDetails = ctx.``[dbo].[OrderDetails]``.Create(cart.AlbumId, order.OrderId, cart.Count, cart.Price)
-        getCart cart.CartId cart.AlbumId ctx
+        let orderDetails = ctx.Public.Orderdetails.Create(cart.Albumid, order.Orderid, cart.Count, cart.Price)
+        getCart cart.Cartid cart.Albumid ctx
         |> Option.iter (fun cart -> cart.Delete())
     ctx.SubmitUpdates()
