fa520c762bd47b4a0ebe6b165d5f75f7bc65d81e
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-01-25 07:47:34 +0100

    Hello World from Suave

---
+++ App.fs
@@ -0,0 +1,4 @@
+open Suave                 // always open suave
+open Suave.Successful      // for OK-result
+
+startWebServer defaultConfig (OK "Hello World!")
--- SuaveMusicStore.fs
+++
@@ -1,6 +0,0 @@
-module SuaveMusicStore
-
-[<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
