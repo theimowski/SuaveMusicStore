092622cb5c11269c2d10e2ec73958090eefdde37
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-01-30 08:08:24 +0100

    Url parameters

--- App.fs
+++ App.fs
@@ -8,7 +8,10 @@ let webPart =
         path "/" >=> (OK "Home")
         path "/store" >=> (OK "Store")
         path "/store/browse" >=> (OK "Store")
-        path "/store/details" >=> (OK "Details")
+        pathScan "/store/details/%d" (fun id -> OK (sprintf "Details: %d" id))
+        pathScan 
+            "/store/details/%s/%d" 
+            (fun (a, id) -> OK (sprintf "Artist: %s; Id: %d" a id))
     ]
 
 startWebServer defaultConfig webPart
