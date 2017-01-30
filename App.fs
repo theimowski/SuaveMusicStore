91ea42204ec1a9c6ac9d24b7c5e6f538375b8899
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-01-30 08:14:35 +0100

    Query parameters

--- App.fs
+++ App.fs
@@ -1,17 +1,21 @@
 open Suave
 open Suave.Filters
 open Suave.Operators
+open Suave.RequestErrors
 open Suave.Successful
+
+let browse =
+    request (fun r ->
+        match r.queryParam "genre" with
+        | Choice1Of2 genre -> OK (sprintf "Genre: %s" genre)
+        | Choice2Of2 msg -> BAD_REQUEST msg)
 
 let webPart = 
     choose [
         path "/" >=> (OK "Home")
         path "/store" >=> (OK "Store")
-        path "/store/browse" >=> (OK "Store")
+        path "/store/browse" >=> browse
         pathScan "/store/details/%d" (fun id -> OK (sprintf "Details: %d" id))
-        pathScan 
-            "/store/details/%s/%d" 
-            (fun (a, id) -> OK (sprintf "Artist: %s; Id: %d" a id))
     ]
 
 startWebServer defaultConfig webPart
