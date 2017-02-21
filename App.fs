7da8abb7bd80db2a24928240606312a4a6e2544d
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-02-21 07:52:32 +0100

    HTML markup

--- App.fs
+++ App.fs
@@ -11,14 +11,14 @@ let html container =
 
 let browse =
     request (fun r ->
-        match r.queryParam "genre" with
+        match r.queryParam Path.Store.browseKey with
         | Choice1Of2 genre -> html (View.browse genre)
         | Choice2Of2 msg -> BAD_REQUEST msg)
 
 let webPart = 
     choose [
         path Path.home >=> html View.home
-        path Path.Store.overview >=> html View.store
+        path Path.Store.overview >=> html (View.store ["Rock"; "Disco"; "Pop"])
         path Path.Store.browse >=> browse
         pathScan Path.Store.details (fun id -> html (View.details id))
         pathRegex "(.*)\.(css|png)" >=> Files.browseHome
