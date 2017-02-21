eaf8eb66004871cc7043d2ac1bd821279b4ccdfa
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-02-21 07:52:32 +0100

    HTML markup

--- View.fs
+++ View.fs
@@ -3,21 +3,34 @@ module SuaveMusicStore.View
 open Suave.Html
 
 let cssLink href = link [ "href", href; " rel", "stylesheet"; " type", "text/css" ]
+let h2 s = tag "h2" [] [Text s]
+let ul nodes = tag "ul" [] nodes
+let li = tag "li" []
 
 let home = [
-    Text "Home"
+    h2 "Home"
 ]
 
-let store = [
-    Text "Store"
+let store genres = [
+    h2 "Browse Genres"
+    p [] [
+        Text (sprintf "Select from %d genres:" (List.length genres))
+    ]
+    ul [
+        for genre in genres ->
+            let url =
+                Path.Store.browse
+                |> Path.withParam (Path.Store.browseKey, genre) 
+            li [ a url [] [ Text genre ] ]
+    ]
 ]
 
 let browse genre = [
-    Text (sprintf "Genre: %s" genre)
+    h2 (sprintf "Genre: %s" genre)
 ]
 
 let details id = [
-    Text (sprintf "Details %d" id)
+    h2 (sprintf "Details %d" id)
 ]
 
 let index container =
