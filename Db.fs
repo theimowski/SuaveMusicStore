4f234e537402579a9ec7c438ba194e5ebcd3d905
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-03-01 07:32:01 +0100

    Queries

--- Db.fs
+++ Db.fs
@@ -19,3 +19,23 @@ type DbContext = Sql.dataContext
 type Album = DbContext.``public.albumsEntity``
 type Genre = DbContext.``public.genresEntity``
 type AlbumDetails = DbContext.``public.albumdetailsEntity``
+
+let getGenres (ctx : DbContext) : Genre list = 
+    ctx.Public.Genres |> Seq.toList
+
+let getAlbumsForGenre genreName (ctx : DbContext) : Album list = 
+    query { 
+        for album in ctx.Public.Albums do
+            join genre in ctx.Public.Genres on (album.Genreid = genre.Genreid)
+            where (genre.Name = genreName)
+            select album
+    }
+    |> Seq.toList
+
+let getAlbumDetails id (ctx : DbContext) : AlbumDetails option = 
+    query { 
+        for album in ctx.Public.Albumdetails do
+            where (album.Albumid = id)
+            select album
+    } 
+    |> Seq.tryHead
