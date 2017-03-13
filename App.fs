0f7b0bb213c2a27aa0318d6fede67330c2a3d415
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-03-13 07:47:20 +0100

    Cart view

--- App.fs
+++ App.fs
@@ -189,6 +189,8 @@ let admin f_success =
         | _ -> UNAUTHORIZED "Not logged in"
     ))
 
+let cart = View.cart [] |> html
+
 let webPart = 
     choose [
         path Path.home >=> html View.home
@@ -203,6 +205,8 @@ let webPart =
 
         path Path.Account.logon >=> logon
         path Path.Account.logoff >=> reset
+
+        path Path.Cart.overview >=> cart
 
         pathRegex "(.*)\.(css|png|gif)" >=> Files.browseHome
         html View.notFound
